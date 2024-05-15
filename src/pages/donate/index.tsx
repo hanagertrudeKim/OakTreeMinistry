cb8fe7255389d1fd7d7ecb1a482817c3c968ec0e
hana <rlagksk8428@gmail.com>
2024-05-15 15:46:19 +0900

    feat: donate page, media page mobile apply

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,19 +1,23 @@
+import useMediaQuery from "@/utils/hooks/useMediaQuery";
 import React from "react";
 
 export default function Donation() {
+  const isMobile = useMediaQuery();
   return (
-    <div className="flex flex-col items-center justify-center py-4 bg-[url('/image/donateBackground.jpg')] h-[93vh] bg-cover">
-      <div className="flex items-start gap-[10vw]">
-        <div className="font-NSK text-white">
-          <div className="text-[65px] font-extrabold indent-3">
-            1:1 청소년 후원하기
+    <div className="flex flex-col items-center justify-center py-4 bg-[url('/image/donateBackground.jpg')] h-[93vh] bg-cover sm:h-[630px] sm:bg-none">
+      <div className="flex items-start gap-[10vw] sm:flex-col">
+        {!isMobile && (
+          <div className="font-NSK text-white">
+            <div className="text-[65px] font-extrabold indent-3">
+              1:1 청소년 후원하기
+            </div>
+            <div className="text-[30px] font-extralight mt-[70px] ml-3">
+              후원하실 프로그램을 선택해주세요.
+            </div>
           </div>
-          <div className="text-[30px] font-extralight mt-[70px] ml-3">
-            후원하실 프로그램을 선택해주세요.
-          </div>
-        </div>
+        )}
         <iframe
-          className="min-w-[425px] max-w-[560px] h-[630px]"
+          className="min-w-[425px] max-w-[560px] h-[630px] sm:mt-10"
           src="https://donorbox.org/embed/oaktree-ministry-1?default_interval=m"
           name="donorbox"
         ></iframe>
--- src/pages/media/index.tsx
+++ src/pages/media/index.tsx
@@ -3,15 +3,17 @@ import React from "react";
 
 export default function index() {
   return (
-    <div className="flex flex-col px-[10vw] py-[5vw]">
-      <div className="text-[45px] font-bold mb-[100px]">OUR RECENT WORK</div>
+    <div className="flex flex-col px-[10vw] py-[5vw] sm:px-[5vw]">
+      <div className="text-[45px] font-bold mb-[100px] sm:text-[30px] sm:mb-[50px]">
+        OUR RECENT WORK
+      </div>
       {/* 방명록 기능 추가 */}
       <Image
         src={"/image/Gallery1.jpg"}
         alt="about-us"
         width={1300}
         height={1100}
-        className="mx-auto sm:w-[90vw] object-contain"
+        className="mx-auto sm:w-[100vw] object-contain"
       />
     </div>
   );
