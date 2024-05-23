e09876666a58f6403a530c64e43483aa9b3367b5
hana <rlagksk8428@gmail.com>
2024-05-24 01:05:58 +0900

    feat: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,39 +1,36 @@
-import React from "react";
+import { NAVIGATION } from '@/static/Navbar';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
-  //임시로 설정
   return (
-    <footer className="w-full font-mono flex flex-col justify-center items-center pt-10 pb-6 transition duration-500 bg-white dark:bg-[#111111] dark:text-white text-black ">
-      <div className="flex justify-center gap-4 items-center pt-4 border-t-2 w-36">
-        <a
-          href="mailto:본인의 이메일 주소 기입"
-          className="hover:scale-110 transition-transform duration-500 hover:text-green-500 hover:fill-green-500 dark:fill-white dark:hover:fill-green-500"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z" />
-          </svg>
-        </a>
-        <a
-          href="본인의 github 주소 기입"
-          className="hover:scale-110 transition-transform duration-500 hover:text-green-500 hover:fill-green-500 dark:fill-white dark:hover:fill-green-500"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-          </svg>
-        </a>
+    <footer className="bg-[#07403A] text-white pt-10 px-[5vw] mt-[10vh]">
+      <div className="container">
+        <div className="flex md:flex-row justify-between">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-xl font-normal">OakTree Ministry</h1>
+          </div>
+          <div className="flex flex-col gap-3">
+            <Link href='/'>
+              <div className="text-white font-bold mx-2">Home</div>
+            </Link>
+            {NAVIGATION.map((nav) => {
+              return (
+                <Link href={nav.link} key={nav.title}>
+                  <div className="text-gray-400 hover:text-white mx-2">{nav.title}</div>
+                </Link>
+              )
+            })}
+          </div>
+          <div className='flex flex-col gap-3'>
+            <Link href="rlagksk8428@gmail.com" className="text-white mx-2 font-semibold">문의하기</Link>
+            <Link href="https://www.instagram.com/oaktreeministryhaiti" className="text-white mx-2 font-semibold">인스타그램</Link>
+          </div>
+        </div>
       </div>
-      <div className="text-sm mt-2">Copyright © 2024</div>
-      <div className="text-xs mt-2">maintain.dev</div>
+      <div className='border-t border-white mt-7 flex justify-end py-3'>
+        <div className='text-gray-100 text-[14px]'>Copyright 2024. oakTree Ministry inc. all right reserved</div>
+      </div>
     </footer>
   );
 }
